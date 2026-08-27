ecab596ea264a15f2a3ba9592ffd27a16d109e5c
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-27 06:33:41 +0200

    feat(DTOs): cria um arquivo de DTOs para shipments

---
+++ DTOs/shipment_DTO.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List
+from typing import Annotated
+
+
+class ShipmentGetResponseDTO(BaseModel):
+    order_id: str
+    shipment_id:str
+    process_docs: bytes
+
+
+class RegistShipmentDTO(BaseModel):
+    order_id: str
+    process_docs: bytes
