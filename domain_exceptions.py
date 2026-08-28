c7b2f01629d14693c02e6f3cd3e8e1ed5ee1315f
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-28 04:51:17 +0200

    feat(domain_exceptions): cria novas classes de erros : ShipmentsError, OrderError - adiciona novos erroe a BAseDomainError:     PERMISSION_DENIED_ERROR     INVALID_OTP_ERROR     ATTEMPTS_EXCEDED_ERROR     INCORRECT_OTP_ERROR - actualiza os erroe de OperatorError

--- domain_exceptions.py
+++ domain_exceptions.py
@@ -6,7 +6,10 @@ class BaseDomainError(Enum):
     EMPTY_TABLE='falha ao buscar dados sua tabela esta vazia'
     DB_ERROR= 'erro no banco de dados'
     DB_CONECTION_ERROR= 'falha ao conectar com o banco de dados'
-
+    PERMISSION_DENIED_ERROR= 'nao tem permissao para executar esta accao'
+    INVALID_OTP_ERROR= 'codogo otp invalido'
+    ATTEMPTS_EXCEDED_ERROR= 'atingiu o limite de tentativas'
+    INCORRECT_OTP_ERROR= 'codigo otp incorrecto'
 
 class BirdsError(Enum):
     BIRD_ALREAD_EXISTS= "Ave ja existente no catalogo"
@@ -14,5 +17,16 @@ class BirdsError(Enum):
 
 
 class OperatorError(Enum):
-    OPERATOR_ALREAD_EXISTS= "O Operador ja se encontra cadastrado"
-    OPERATOR_DUPLICATE_EMAIL= "ja existe um poerador ja cadastrado com o email fornecido"
+    OPERATOR_ALREAD_EXISTS_ERROR= "O Operador ja se encontra cadastrado"
+    OPERATOR_NOT_FOUND_ERROR= 'operador nao encontrado'
+    OPERATOR_DUPLICATE_EMAIL_ERROR= "o email fornecido ja se encontra cadastrado"
+    OPERATOR_DUPLICATE_TELEFONE_ERROR= 'o numero telefonico ja se encontra cadastrado'
+    OPERATOR_DUPLICATE_BI_ERROR= 'o BI fornecido ja se encontra cadastrado'
+
+
+class ShipmentsError(Enum):
+    SHIPMENT_NOT_FOUND_ERROR= 'nao foram encontradas exportacoes correspondentes'
+
+
+class OrderError(Enum):
+    ORDER_NOT_FOUND_ERROR= 'nao foram encontrados pedidos correspondentes'
