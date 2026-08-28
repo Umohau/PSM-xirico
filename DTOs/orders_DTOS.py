a7bba1a614f95a2bffb75b149f698433643dae8f
Umohau Haraba <xiricoaviva@gmail.com>
2026-08-28 04:13:11 +0200

    feat(orders_DTOS): substitue o uso de dataclasses puros pelo pydantic e cria tipos com o Annotated remove dataclasses repetidos

--- DTOs/orders_DTOS.py
+++ DTOs/orders_DTOS.py
@@ -1,35 +1,71 @@
-import dataclasses
+from pydantic import BaseModel, Field
+from typing import Annotated, Optional
 from datetime import date
 
-@dataclasses.dataclass
-class DoneOrdersGetResponse:
-    order_id: str
-    date_of_regist: date
-    sent_date: date
-    manager: int
-    client: int
+# tipos
+ORDER_ID= Annotated[
+    str,
+    Field(
+        title='order id',
+        description="id do pedido, texto que comeca com 'ORD'",
+        examples='ORD123',
+        alias='id'
+    )
+]
+
+DATE= Annotated[
+    date,
+    Field(
+        title='date of regist',
+        description= 'data de registro do pedido',
+        examples=['2026/05/15']
+    )
+]
+
+BIRD= Annotated[
+    int,
+    Field(
+        title= 'bird id',
+        description= 'id de registro da ave',
+        examples=12
+    )
+]
+
+STATUS= Annotated[
+    str, 
+    Field(
+        min_length=4,
+        max_length=8,
+        title='status',
+        description='estado do pedido',
+        examples=['status= Pending']
+    )
+]
+
+class OrderGetResponseDTO(BaseModel):
+    order_id: ORDER_ID
+    date_of_regist: DATE
+    sent_date: Optional[DATE]= None
+    menager_id: int
+    client_id: int
     quantity: int
-    bird: str
-    status: str= 'Done'
+    bird: BIRD
+    order_status: STATUS
 
-
-@dataclasses.dataclass
-class PendingOrdersGetResponse:
-    order_id: str
-    date_of_regist: date
-    manager: int
-    client: int
+class OrderRegistDTO(BaseModel):
+    date_of_regist: DATE
+    menager_id: int
+    client_id: int
     quantity: int
-    bird: str
-    status: str= 'Pending'
+    bird: BIRD
 
 
-@dataclasses.dataclass
-class CanceledOrdersGetResponse:
-    order_id: str
-    date_of_regist: date
-    manager: int
-    client: int
-    quantity: int
-    bird: str
-    status: str= 'canceled'
+class OrderUpdateDTO(BaseModel):
+    order_id: ORDER_ID
+    date_of_regist: Optional[DATE]= None
+    sent_date: Optional[DATE]= None
+    menager_id: Optional[int]= None
+    client_id: Optional[int]= None
+    quantity: Optional[int]=None
+    bird: Optional[BIRD]=None
+    order_status: Optional[STATUS]= None
